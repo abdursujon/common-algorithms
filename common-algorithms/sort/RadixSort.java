3278394b7dbf8162e556d9bf125e56aaa88de7a1
Sujon <abdur.sujon.dev@gmail.com>
2026-06-17 16:02:41 +0100

    radix sort added, needs commenting and review

--- common-algorithms/sort/RadixSort.java
+++ common-algorithms/sort/RadixSort.java
@@ -1,7 +1,41 @@
 package sort;
 
-public class RadixSort{
-    public static void main(String[] args){
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class RadixSort{
+
+    public static int[] radixSort(int[] nums){
+        int maxNumberOfDigits = String.valueOf(Arrays.stream(nums).max().getAsInt()).length();
+
+        for(int d = 0; d < maxNumberOfDigits; d++){
+            List<List<Integer>> buckets = new ArrayList<>();
+            for(int k = 0; k < 10; k++){
+                buckets.add(new ArrayList<>());
+            }
+
+            for(int n: nums){
+                int digit = (n / (int) Math.pow(10, d)) % 10;
+                buckets.get(digit).add(n);
+            }
+
+            int i = 0;
+            for(List<Integer> bucket: buckets){
+                for(int n: bucket){
+                    nums[i] = n;
+                    i++;
+                }
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {5,2,4,1,3,6,0};
+        System.out.println(Arrays.toString(radixSort(nums)));
+
+        int[] nums2 = {5,2,4,1,3,6,0, 5,2,4,1,3,6,0, 5,2,4,1,3,6,0, 5,2,4,1,3,6,0, 90, 55, 11, 22, 33};
+        System.out.println(Arrays.toString(radixSort(nums2)));
     }
 }
